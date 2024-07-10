d17de5a715e5378eda9d2baebd2242eef039bc94
idah-github <guantaiidah@gmail.com>
2024-07-10 10:15:34 +0000

    asy debug

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python3
-"""
-measure_runtime coroutine that will execute measure_time
-4 times in parallel using asyncio.gather
-"""
-
-import asyncio
-import time
+'''
+coroutine async_comprehension taking no arguments
+'''
+from typing import List
 from importlib import import_module as using
 
 
-async_comprehension = using('1-async_comprehension').async_comprehension
+async_generator = using('0-async_generator').async_generator
 
 
-async def measure_runtime() -> float:
-    '''Executes async_comprehension 4 times and measures the
-    total execution time.
+async def async_comprehension() -> List[float]:
+    '''Creates a list of 10 numbers from a 10-number generator.
     '''
-    start_time = time.time()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.time() - start_time
+    return [num async for num in async_generator()]
